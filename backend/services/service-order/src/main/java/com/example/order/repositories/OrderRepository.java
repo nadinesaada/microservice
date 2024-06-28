6418b798d446013d37ec3d9011c265ca3f114455
yasmine-trabelsi <126244083+yasmine-trabelsi@users.noreply.github.com>
2024-06-28 08:32:23 +0100

    Update OrderRepository.java

--- backend/services/service-order/src/main/java/com/example/order/repositories/OrderRepository.java
+++ backend/services/service-order/src/main/java/com/example/order/repositories/OrderRepository.java
@@ -4,13 +4,7 @@ import org.springframework.stereotype.Repository;
 import com.example.order.entities.OrderEntity;
 
 import java.util.Optional;
-/**
- * This interface represents the repository for managing OrderEntity objects.
- * It extends the JpaRepository interface which provides basic CRUD operations
- * for the OrderEntity class and allows us to easily perform database operations.
- *
- * @author Mejda Sliman _ mejda.sliman@esprit.tn 2ALINFO3
- */
+
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
    OrderEntity findByNameAndPrice(String name, String price);
